da03877b2fe05436e0c2ef1874e8340e885ef725
yolandaayuso <yolanda.ayuso@alu.uclm.es>
2025-04-29 19:33:59 +0200

    nuevo

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/electricidadBogalo/',  // <-- Añadido para GitHub Pages
+  base: '/electricidadBogalov.1.2/',  // <-- Corrección aquí
   server: {
     host: "::",
     port: 8080,
